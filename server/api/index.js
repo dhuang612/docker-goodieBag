47814a3834e15a810e835bacdb9678130dc6d0bc
dhuang612 <dhuang@madisoncollege.edu>
2020-08-31 10:33:39 -0400

    created candy model, added express route

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Candy = require('../db/models/Candy')
+
 const router = require('express').Router()
 
 // Your routes go here!
@@ -18,6 +20,17 @@ const router = require('express').Router()
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
+
+router.use('/candy', async (req, res, next) => {
+  try {
+    const allCandy = await Candy.findAll();
+    res.send(allCandy);
+  } catch (error) {
+    next(error);
+  }
+})
+
+
 router.use((req, res, next) => {
   const err = new Error('API route not found!')
   err.status = 404
